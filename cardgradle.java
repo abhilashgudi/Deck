0d4d8594733a11807939408e530a394beaff0bb8
abhilashgudi <33881862+abhilashgudi@users.noreply.github.com>
2017-11-22 14:17:22 -0500

    first git commit
    deck of cards

---
+++ cardgradle.java
@@ -0,0 +1,28 @@
+/**
+ * Card class which represents a single card
+ */
+public class Card {
+    private Suits suit;
+    private FaceValues faceValues;
+
+    public Card(Suits suit, FaceValues faceValues) {
+        this.suit = suit;
+        this.faceValues = faceValues;
+    }
+
+    //getter methods
+    public Suits getSuit() {
+        return suit;
+    }
+
+    public FaceValues getFaceValue() {
+        return faceValues;
+    }
+
+    //converting the given card into string
+    @Override
+    public String toString() {
+        // TODO Auto-generated method stub
+        return suit+"-"+faceValues;
+    }
+}
